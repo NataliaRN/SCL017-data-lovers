c57f5f221a5f64117e91b7029982a5a0c4b63b51
yuritalinda <yuritalinda@gmail.com>
2021-06-25 18:18:58 -0400

    barra de busqueda lista

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import datosRyM from './data.js';
 
-console.log(datosRyM);
 
 
 
@@ -21,13 +20,29 @@ lupa.addEventListener("click", filtraNombre);
 function filtraNombre(){
 
   let catchInput = (document.getElementById("input-search").value);
-  let textCatchInput = catchInput.charAt(0).toUpperCase() + catchInput.slice(1).toLowerCase();
+  
+  let arrayNombres = datosRyM.filterByName(catchInput);
+  console.log(arrayNombres); //este array se trae todo el objeto
+
+  if(arrayNombres.length === 0){
+    console.log("El objeto está vacío");  //Hacer mensaje de error dinámico y resetear el input
+  };
+
+  for(let i = 0 ; i<arrayNombres.length ;  i++){ //Iteracción para sacar la imagen y el nombre
+
+    let ficha_imagen = arrayNombres[i].image;
+    let ficha_nombre =  arrayNombres[i].name;
 
 
+    console.log(ficha_imagen , ficha_nombre);
 
-  console.log(catchInput , textCatchInput , "funciona la función");
+
+  }
+
+
 
 
 }
+
 
 
