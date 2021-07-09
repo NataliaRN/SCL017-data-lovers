005e01c8f6496cff87470093a52a67b8f3df7d12
yuritalinda <yuritalinda@gmail.com>
2021-07-09 16:07:29 -0400

    test unitarios de funciones en progreso

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,41 @@
-import { example, anotherExample } from '../src/data.js';
+import datosRyM from '../src/data';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('filterByName', () => {
+  it('is a function', () => {    
+    expect(typeof datosRyM.filterByName).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it.skip('returns the correct character(s)', () => {    
+    // Arrange
+      // Preparar los datos de entrada y lo que esperamos de vuelta de la función
+     
+
+    // Act
+      // Ejecutar la función, atrapando el valor de retorno
+
+    // Assert
+      // Comparar el valor de retorno de la función con el valor esperado inicializado en la etapa de Arrange
   });
 });
 
 
-describe('anotherExample', () => {
+describe('ordenAZ', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof datosRyM.ordenAZ).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it.skip('returns `datosRyM.ordenAZ`', () => {
+    expect(datosRyM.ordenAZ()).toBe('OMG');
+  });
+});
+
+describe('ordenZA', () => {
+  it('is a function', () => {
+    expect(typeof datosRyM.ordenZA).toBe('function');
+  });
+
+  it.skip('returns `datosRyM.ordenZA`', () => {
+    expect(datosRyM.ordenZA()).toBe('OMG');
   });
 });
