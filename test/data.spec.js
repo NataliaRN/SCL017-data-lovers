a168a9af9ff1217c1b58acb9be3e3832c3882843
NataliaRN <83964278+NataliaRN@users.noreply.github.com>
2021-07-10 16:23:22 -0400

    Revert "Últimos Cambios"

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,41 +1,23 @@
-import datosRyM from '../src/data';
+import { example, anotherExample } from '../src/data.js';
 
 
-describe('filterByName', () => {
-  it('is a function', () => {    
-    expect(typeof datosRyM.filterByName).toBe('function');
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
   });
 
-  it.skip('returns the correct character(s)', () => {    
-    // Arrange
-      // Preparar los datos de entrada y lo que esperamos de vuelta de la función
-     
-
-    // Act
-      // Ejecutar la función, atrapando el valor de retorno
-
-    // Assert
-      // Comparar el valor de retorno de la función con el valor esperado inicializado en la etapa de Arrange
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
   });
 });
 
 
-describe('ordenAZ', () => {
+describe('anotherExample', () => {
   it('is a function', () => {
-    expect(typeof datosRyM.ordenAZ).toBe('function');
+    expect(typeof anotherExample).toBe('function');
   });
 
-  it.skip('returns `datosRyM.ordenAZ`', () => {
-    expect(datosRyM.ordenAZ()).toBe('OMG');
-  });
-});
-
-describe('ordenZA', () => {
-  it('is a function', () => {
-    expect(typeof datosRyM.ordenZA).toBe('function');
-  });
-
-  it.skip('returns `datosRyM.ordenZA`', () => {
-    expect(datosRyM.ordenZA()).toBe('OMG');
+  it('returns `anotherExample`', () => {
+    expect(anotherExample()).toBe('OMG');
   });
 });
